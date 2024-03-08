100f1fffbbc18961497be517ba9d938dfbf6851e
bishmop <stacybishop35@gmail.com>
2024-03-08 16:59:32 -0500

    fr FR

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -87,7 +87,10 @@ var config_data = `
       "code": "tss",
       "type": "counter"
     },
-
+    { "name": "Fed Notes",
+      "code": "tko",
+      "type": "counter"
+    },
     { "name": "Pickup From",
       "code": "tpu",
       "type": "radio",
@@ -99,10 +102,7 @@ var config_data = `
       },
       "defaultValue": "x"
     },
-    { "name": "Fed Notes",
-      "code": "abl",
-      "type": "counter"
-     },
+
   ],
 
   "endgame": [
