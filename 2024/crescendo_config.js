a06af8f045e6621458c2940695922df891d2f324
bishmopp <105512272+bishmopp@users.noreply.github.com>
2024-04-11 16:10:36 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024MICMP2",
+      "defaultValue": "2024NECMP1",
       "required": "true"
     },
     { "name": "Match Level",
